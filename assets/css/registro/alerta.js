76a01800dae28630931e59be8f4cd56f21f23101
Miguel-Angel-Rincon <angelrinconorozco11@gmail.com>
2024-05-29 20:56:18 -0500

    cambios

--- assets/css/registro/alerta.js
+++ assets/css/registro/alerta.js
@@ -1,5 +1,23 @@
-window.addEventListener("load", function() {
-    document.getElementById("viewalert").addEventListener("click", function() {
-        alert("Tu registro ha sido correcto");
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  let alertPlaceholder = document.getElementById("alerts");
+  function appendAlert(message, type) {
+    let wrapper = document.createElement("article");
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible fade show m-2" role="alert">`,
+      `   <span>${message}</span>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      "</div>",
+    ].join("");
+
+    if (alertPlaceholder) {
+      alertPlaceholder.append(wrapper);
+      setTimeout(() => {
+        location.href = "login.html";
+      }, 8000);
+    }
+  }
+  document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    appendAlert("Registro exitoso", "success");
+  });
+});
